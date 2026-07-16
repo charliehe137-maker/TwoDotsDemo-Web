f4c5391b661928fc3f09d86a0ff610b3d72530fc
Administrator <admin@example.com>
2026-07-16 22:30:38 +0800

    deploy: update playable demo with tutorials and revised levels

--- application.js
+++ application.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.02f29.json';
+          this.settingsPath = 'src/settings.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
